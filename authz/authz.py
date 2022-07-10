c3c7b79caa95da92eca63345ca6191e3caa6c5b9
majid roodi <majidroodi.mr@gmail.com>
2022-07-10 22:13:54 +0430

    add authz stracture file

--- authz/authz.py
+++ authz/authz.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import Flask, Blueprint 
+from flask_restful import Api
 from authz.config import Config
+
+apiv1_bp = Blueprint("apiv1_bp", __name__, url_defaults="/api/v1")
+ap1v1 = Api(apiv1_bp)
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
-    print(app.config)
     return app 
- 
