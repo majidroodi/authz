933579a182b57ab3c63ff8c28f8c8a248f6a05cd
majid roodi <majidroodi.mr@gmail.com>
2022-07-11 01:10:08 +0430

    adding user resource and user controller prototype

---
+++ authz/controller/apiv1/user.py
@@ -0,0 +1,13 @@
+from authz.util import jsonify
+
+class UserController:
+    def get_users():
+        pass
+    def get_user(user_id):
+        pass
+    def create_user():
+        pass
+    def update_user(user_id):
+        pass
+    def delete_user(user_id):
+        pass
