30aee4f6a2efe00dd46cc2551386efc52af84832
majid roodi <majidroodi.mr@gmail.com>
2022-07-11 01:13:41 +0430

    update user resource

--- authz/resource/apiv1/user.py
+++ authz/resource/apiv1/user.py
@@ -1,10 +1,15 @@
 from flask_restful import Resource
+from authz.controller.apiv1 import UserController
+
 class UserResource(Resource):
     def get(self, user_id=None):
-        pass
+        if user_id is None:
+            return UserController.get_users()
+        else:
+            return UserController.get_user(user_id)
     def post(self):
-        pass
+        return UserController.create_user()
     def patch(self, user_id):
-        pass
+        return UserController.update_user(user_id)
     def delete(self, user_id):
-        pass
+        return UserController.delete_user(user_id)
