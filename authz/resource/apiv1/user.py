9ab61e85450c66c839f54ea817e817647298280c
majid roodi <majidroodi.mr@gmail.com>
2022-07-11 00:30:20 +0430

    add user file

---
+++ authz/resource/apiv1/user.py
@@ -0,0 +1,10 @@
+from flask_restful import Resource
+class UserResource(Resource):
+    def get(self, user_id=None):
+        pass
+    def post(self):
+        pass
+    def patch(self, user_id):
+        pass
+    def delete(self, user_id):
+        pass
