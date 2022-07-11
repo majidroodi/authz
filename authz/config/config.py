3a0fccc42c6ee514a1d30e0cde46711332eb2601
majid roodi <majidroodi.mr@gmail.com>
2022-07-12 01:01:19 +0430

    add database local config

--- authz/config/config.py
+++ authz/config/config.py
@@ -2,7 +2,13 @@ from mimetypes import init
 from os import environ
 
 class Config:
+    ########### Application Config ##########
     ENV = environ.get("TECHLAND_AUTHZ_ENV", "production")
     DEBUG = bool(init(environ.get("TECHLAND_AUTHZ_DEBUG", "0")))
     TESTING = bool(init(environ.get("TECHLAND_AUTHZ_TESTING", "0")))
     SECRET_KEY = environ.get("TECHLAND_AUTHZ_SECRET_KEY", "HARD-HARD-HARD-SECRET-KEY")
+    ############ Database Config ############
+    SQLALCHEMY_DATABASE_URI = environ.get("TECHLAND_AUTHZ_DATABASE_URI", None)
+    SQLALCHEMY_ECHO = DEBUG
+    SQLALCHEMY_RECORD_QUERIES = DEBUG
+    SQLALCHEMY_TRACK_MODIFICATIONS = DEBUG
