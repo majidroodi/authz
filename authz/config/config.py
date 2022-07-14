4002313dbee5403b7f7b7a3342666399428f2bcd
majid roodi <majidroodi.mr@gmail.com>
2022-07-14 13:34:59 +0430

    adding timezone config to config.py

--- authz/config/config.py
+++ authz/config/config.py
@@ -7,6 +7,7 @@ class Config:
     DEBUG = bool(init(environ.get("TECHLAND_AUTHZ_DEBUG", "0")))
     TESTING = bool(init(environ.get("TECHLAND_AUTHZ_TESTING", "0")))
     SECRET_KEY = environ.get("TECHLAND_AUTHZ_SECRET_KEY", "HARD-HARD-HARD-SECRET-KEY")
+    TIMEZONE = environ.get("TECHLAND_AUTHZ_TIMEZONE", "Asia/Tehran")
     ############ Database Config ############
     SQLALCHEMY_DATABASE_URI = environ.get("TECHLAND_AUTHZ_DATABASE_URI", None)
     SQLALCHEMY_ECHO = DEBUG
